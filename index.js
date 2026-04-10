97dbd1832fdae91aded6a93ca6fbe9f58a7b6810
tom <tomgacami@gmail.com>
2026-04-10 18:11:21 -0300

    Exercise 3.1 step 1

---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log('Hello mundossssssssssssssssssssss')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const PORT = 3001
+app.listen(PORT,() => {
+    console.log(`Server running in port ${PORT}`)
+})
+
+const phonebook =
+    [
+        {
+            "id": 1,
+            "name": "Arto Hellas",
+            "number": "040-123456"
+        },
+        {
+            "id": 2,
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523"
+        },
+        {
+            "id": 3,
+            "name": "Dan Abramov",
+            "number": "12-43-234345"
+        },
+        {
+            "id": 4,
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122"
+        }
+    ]
+// app.get('/', (request, response) =>{
+//     response.send('<h1>Hello worldssssssssssssssssssssss</h1>')
+// })
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
