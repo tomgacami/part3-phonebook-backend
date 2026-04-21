215e09355b613e4a140b0d95afd30fd5054adbee
tom <tomgacami@gmail.com>
2026-04-21 11:59:11 -0300

    Exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,57 @@
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log('Give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+// let number =0
+// process.argv.forEach((param,number) => {
+//
+//     console.log(`posicion ${number} is ${param}`)
+//     number = +1
+// })
+
+
+
+const url =
+    `mongodb+srv://admin_db_user:${password}@cluster0.yijnldi.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+//PRINT PERSONS
+if (process.argv.length === 3){
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.phone}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    //ADD PERSON
+    const name = process.argv[3]
+    const phone = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        phone: phone
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${phone} to phonebook`)
+        mongoose.connection.close()
+    })
+}
